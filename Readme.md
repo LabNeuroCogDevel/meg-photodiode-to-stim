9b7a28991a74d80d4b55c7dfa02fd815e6088d9d
Jason Schuchardt <jargon.schuchardt@gmail.com>
2016-06-03 06:15:58 -0400

    added a readme file, and some comments in fifproc.py

---
+++ Readme.md
@@ -0,0 +1,11 @@
+#ASSUMPTIONS ABOUT THE DATA
+* Stim channel changes **BEFORE** photodiode changes
+* Zero values in stim channel should be overwritten with the last  
+  value of the stim channel
+* Distances between photodiode values on adjacent screens are much  
+  larger than variation in photodiode value on a single screen
+#DEFAULT VALUES
+* Input stim channel is STI101
+* Input photodiode channel is MISC007
+* Output stim channel is STI101
+* Reasonable thresholding value for the photodiode data is 0.4
--- fifproc.py
+++ fifproc.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 
+# ASSUMPTIONS ABOUT THE DATA
+# - Stim channel changes BEFORE photodiode changes
+# - Zero values in stim channel should be overwritten with the last
+#   value of the stim channel
+# - Distances between photodiode values on adjacent screens are much
+#   larger than variation in photodiode value on a single screen
+# DEFAULT VALUES
+# - Input stim channel is STI101
+# - Input photodiode channel is MISC007
+# - Output stim channel is STI101
+# - Reasonable thresholding value for the photodiode data is 0.4
+
 from __future__ import print_function
 
 import re
